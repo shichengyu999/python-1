9aaeb3d618f18675c74358871edc8294cd01c2e1
scy <shichengyu999@yahoo.com>
2016-10-16 10:09:51 +0800

    这只爬虫可以爬取快代理的代理池，进一步其实是想要进化成一个函数，方便以后爬虫调用

---
+++ ip代理爬取.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+#settings
+ad ='E:/python/newspaper/'
+
+#time
+import datetime
+now = datetime.datetime.now()
+now =  now.strftime('%Y-%m-%d')
+
+#time and name
+adress = ad+now+'.txt'
+
+#codeing
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+#import
+import urllib2,urllib,re,os,lxml
+import lxml.etree as etree
+
+#catch html
+re1 = urllib2.urlopen("http://www.kuaidaili.com/").read()
+
+#search html
+re1 = lxml.etree.HTML(re1)
+re2 = re1.xpath('//table[@class="table table-bordered table-striped  table-index"]/tbody/tr/td[@data-title="IP"]|//table[@class="table table-bordered table-striped  table-index"]/tbody/tr/td[@data-title="PORT"]')
+
+# save html
+N = 1
+for re3 in re2:
+    N = N + 1
+    with open(adress,'a') as f:
+        f.write(re3.text)
+        while N % 2 ==1:
+            f.write('\n')
+
+print "finish"
