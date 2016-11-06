3116ca51352c68123c3f9293bb434a5228a69f78
scy <shichengyu999@yahoo.com>
2016-11-06 20:06:36 +0800

    It's a surprising commit. It's the first program that I have built by flask and and be surf by Internet. What a surprise!

--- ip代理爬取.py
+++ ip代理爬取.py
@@ -11,7 +11,8 @@ now = datetime.datetime.now()
 now =  now.strftime('%Y-%m-%d')
 
 #time and name
-adress = ad+now+'.txt'
+adress = ad+now+'dl'+'.txt'
+adress2 = ad+now+'xw'+'.txt'
 
 #codeing
 import sys
@@ -23,19 +24,56 @@ import urllib2,urllib,re,os,lxml
 import lxml.etree as etree
 
 #catch html
-re1 = urllib2.urlopen("http://www.kuaidaili.com/").read()
+dre1 = urllib2.urlopen("http://www.kuaidaili.com/").read()
 
 #search html
-re1 = lxml.etree.HTML(re1)
-re2 = re1.xpath('//table[@class="table table-bordered table-striped  table-index"]/tbody/tr/td[@data-title="IP"]|//table[@class="table table-bordered table-striped  table-index"]/tbody/tr/td[@data-title="PORT"]')
+dre1 = lxml.etree.HTML(dre1)
+dre2 = dre1.xpath('//table[@class="table table-bordered table-striped  table-index"]/tbody/tr/td[@data-title="IP"]|//table[@class="table table-bordered table-striped  table-index"]/tbody/tr/td[@data-title="PORT"]')
 
 # save html
+with open(adress,'w') as f:
+    f.write('\n')
 N = 1
-for re3 in re2:
-    with open(adress,'a') as f:
-        f.write(re3.text)
+for dre3 in dre2:
+    with open(adress,'a+') as f:
+        f.write(dre3.text)
         f.write('\n')
 print "finish"
 
-#read html
+with open(adress,'a+') as f:
+    passage1 = f.read()
 
+
+
+
+
+#top爬虫
+#catch html
+re1 = urllib2.urlopen("http://top.baidu.com/").read()
+
+#search html
+re1 = lxml.etree.HTML(re1)
+re2 = re1.xpath('//ul[@id="hot-list"][@class="list"]/*/a[1]')
+with open(adress2,'w') as f:
+    f.write('\n')
+#save hrml
+for re3 in re2:
+    with open(adress2,'a') as f:
+        f.write(re3.text)
+        f.write('\n')
+    print 'ok'
+with open(adress2,'a+') as f:
+    passage2 = f.read()
+
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/ip')
+def hello_world():
+    return  passage1
+@app.route('/')
+def xw():
+    return passage2
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',debug=True)
